e8de6d947e01b75fb0d5dfa240a94488396b7e54
Azimjon Pulatov <azimjon@mysuperdispatch.com>
2019-10-06 15:57:27 +0500

    add healthcheck

--- spider/urls.py
+++ spider/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('api/', include(('music.urls', 'music'))),
+    path('', lambda request: HttpResponse('{"alive": true}')),
 ]
